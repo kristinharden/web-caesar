38e15668793d748854caf9ab4c6aa64bd61cfefd
Kristin <KristinHarden@gmail.com>
2017-12-15 16:49:25 -0500

    completed instructions through implementation of caesar.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from caesar import rotate_string
 
 app = Flask(__name__)
@@ -32,20 +32,26 @@ form = """
             <input id="rot_input" type="text" name="rot" value="0"/>
             <br>
             <br>
-            <textarea>Please type your message here.</textarea>
+            <textarea name="text">Please type your message here.</textarea>
             <br>
             <br>
             <input type="submit" value="Submit Query" />
         </form>
     </body>
 </html>
-
 """
-
-
 
 @app.route("/")
 def index():
     return form
+
+@app.route("/", methods=["POST"])
+def encrypt():
+    rot = int(request.form["rot"])
+    text = request.form["text"]
+    rotated_message = rotate_string(text, rot)
+
+    return "<h1>" + rotated_message + "</h1>"
+
 
 app.run()
